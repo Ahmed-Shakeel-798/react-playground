af51669e1ab1afed1c4ea6a5c709908d1cc6c774
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2023-01-20 00:48:43 +0500

    JS expressions in JSX

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Ahmed";
+const lName = "Shakeel";
+
+// javascriot expressions in JSX
+
 ReactDOM.render(
   <div>
-    <h1>My Favorite Books</h1>
-    <ul>
-      <li>Ego is the enemy</li>
-      <li>1984</li>
-      <li>Notes from underground</li>
-    </ul>
+    <h1>Lorem ipsum</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Created by {fName} {lName}</p>
+    <p>Copyright {new Date().getFullYear()}</p>
   </div>,
   document.getElementById("root")
 );
