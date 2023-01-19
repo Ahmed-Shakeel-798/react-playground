e6e63d73b84ee1da2b5ca61147ebc8086180f33b
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2023-01-20 00:22:32 +0500

    init commit

---
+++ react-basic/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Books</h1>
+    <ul>
+      <li>Ego is the enemy</li>
+      <li>1984</li>
+      <li>Notes from underground</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+// what to show, where to show
