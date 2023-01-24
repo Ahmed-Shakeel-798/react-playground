850750ef151ecf1646a9cb5589fe93229918ef71
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2023-01-25 00:16:28 +0500

    JSX attributes

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -4,13 +4,19 @@ import ReactDOM from "react-dom";
 const fName = "Ahmed";
 const lName = "Shakeel";
 
-// javascriot expressions in JSX
+// javascript expressions in JSX
+// JSX attributes use camlecased version of html global attribiutes
+
+const image1 = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7cYcH4m0yMeufIWDWdsIZgHaFj%26pid%3DApi&f=1&ipt=8113681082c10d915f0c4cd1d2e86989d4b694405debaa8471f98efaafa165b7&ipo=images";
+const image2 = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.QRzC-RJHu1bkOi9K-80C-AAAAA%26pid%3DApi&f=1&ipt=987c29d77f72e1ab91005ebcc5e1df78ef1521cf073d825733bb6b370fce83c0&ipo=images";
+const image3 = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.rQLhQOuMDbv9pyFu0vf-xwHaLX%26pid%3DApi&f=1&ipt=9ea1a55c3e2823c4addc955832455b71cc890e2902ad8b3ccee6d015e13715d8&ipo=images";
 
 ReactDOM.render(
   <div>
-    <h1>Lorem ipsum</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h1 className="heading">My favorite books</h1>
+    <img className="bookImages" src={image1}></img>
+    <img className="bookImages" src={image2}></img>
+    <img className="bookImages" src={image3}></img>
     <p>Created by {fName} {lName}</p>
     <p>Copyright {new Date().getFullYear()}</p>
   </div>,
