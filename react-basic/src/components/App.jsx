61c58ad4aa37386614a0bb2ab3635eebdcb66822
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2023-01-29 01:43:47 +0500

    props

--- react-basic/src/components/App.jsx
+++ react-basic/src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import Note from "./Note";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
-    return <div>
-        <Header /> 
-        <Footer />
-        <Note />
-    </div>;
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card name={contacts[0].name} imgURL={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email}/>
+      <Card name={contacts[1].name} imgURL={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email}/>
+      <Card name={contacts[2].name} imgURL={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email}/>
+    </div>
+  );
 }
 
 export default App;
